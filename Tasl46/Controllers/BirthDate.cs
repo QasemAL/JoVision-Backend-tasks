26e625ca7b6ada6dd8a33b67aaa156a8ee40d590
QasemAL <129743072+QasemAL@users.noreply.github.com>
2024-08-04 09:59:07 +0300

    Task46

---
+++ Tasl46/Controllers/BirthDate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task45.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthDateController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Get([FromBody] Person person)
+        {
+            string greetName = string.IsNullOrEmpty(person.name) ? "anonymous" : person.name;
+
+            if (person.years != null && person.months != null && person.days != null)
+            {
+                try
+                {
+                    DateTime birthDate = new DateTime(person.years, person.months, person.days);
+                    int age = CalculateAge(birthDate);
+                    string message = $"Hello {greetName}, your age is {age}";
+                    return Ok(message);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    return BadRequest($"Hello {greetName}, the provided date is invalid.");
+                }
+            }
+            else
+            {
+                return BadRequest($"Hello {greetName}, I can’t calculate your age without knowing your full birthdate.");
+            }
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            var today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public int years { get; set; }
+        public int months { get; set; }
+        public int days { get; set; }
+    }
+}
---
+++ Tasl46/Controllers/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task45.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Get([FromBody] Person person)
+        {
+            string greetname = string.IsNullOrEmpty(person.name) ? "anonymous" : person.name;
+            return Ok($"hello {greetname}");
+        }
+    }
+
+}
